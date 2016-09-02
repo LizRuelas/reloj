061ec62e5a5b826789ad340301f0f2b6be8b71c5
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-02 16:02:22 -0700

    iniciando repo

---
+++ app.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function() {
+
+	function mueveReloj(){ 
+	   	momentoActual = new Date() ;
+	   	hora = momentoActual.getHours() ;
+	   	minuto = momentoActual.getMinutes() ;
+	   	segundo = momentoActual.getSeconds() ;
+
+	   	minuto = cero(minuto);
+	   	segundo = cero(segundo);
+
+	   	horaImprimible = hora + " : " + minuto + " : " +segundo ;
+	   	
+	   	var resultado = document.getElementById("resultado");
+		resultado.innerHTML = "<p>" + horaImprimible + "</p>";
+
+		var resultadoArgentina = document.getElementById("resultadoArgentina");
+		resultadoArgentina.innerHTML = (hora+14) + " : " + minuto + " : " + segundo;
+
+	   	setTimeout(mueveReloj,1000) ;
+	} 
+
+	function cero(i) {
+		if (i<10) { i="0" +i};
+		return i; 
+	}
+
+	mueveReloj();
+});
+
+
+
+
+
+
